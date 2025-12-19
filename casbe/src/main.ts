5e1dc93b3c04d315101b4d47105cee28e39bb707
Bibek Ale Magar <magarbibek71@gmail.com>
2025-12-19 21:54:38 +0545

    applies login through jwt

--- casbe/src/main.ts
+++ casbe/src/main.ts
@@ -8,6 +8,11 @@ import { createSuperAdmin } from './seed/superAdmin.seed';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.enableCors({
+    origin: "*",
+    methods: ['GET', 'POST', 'PATCH', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: "*",
+  })
   const dataSource = app.get(DataSource);
   const config = new DocumentBuilder()
     .setTitle('API')
