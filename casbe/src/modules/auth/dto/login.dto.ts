55837fa59b06204bc4d411b5c241c550a807d6ba
Bibek Ale Magar <magarbibek71@gmail.com>
2025-12-19 08:21:31 +0545

    starts frontend

--- casbe/src/modules/auth/dto/login.dto.ts
+++ casbe/src/modules/auth/dto/login.dto.ts
@@ -1,12 +1,15 @@
+import { ApiProperty } from "@nestjs/swagger";
 import { IsEmail, IsNotEmpty, IsString } from "class-validator";
 
 
 export class LoginDto  {
+    @ApiProperty({example: "superdmin@xyz.com"})
     @IsEmail()
     @IsNotEmpty()
     email: string;
 
 
+    @ApiProperty({example: "yourpassword"})
     @IsString()
     @IsNotEmpty()
     password: string;
