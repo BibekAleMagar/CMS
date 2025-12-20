23cf5e592b2bd3ede03ea892ff7e15590c0d1605
Bibek Ale Magar <magarbibek71@gmail.com>
2025-12-20 20:42:56 +0545

    implemensts register in frontend and backend

--- casbe/src/modules/auth/dto/register.dto.ts
+++ casbe/src/modules/auth/dto/register.dto.ts
@@ -1,32 +1,40 @@
-import { IsEmail, IsEnum, IsNotEmpty, IsString, MinLength } from "class-validator";
+import { ApiProperty } from "@nestjs/swagger";
+import { IsEmail, IsEnum, IsNotEmpty, IsOptional, IsString, MinLength } from "class-validator";
 import { UserRole } from "src/common/enums/user-role.enum";
 
 export class RegisterDto {
 
+    @ApiProperty({ example: "user@example.com" })
     @IsEmail()
     @IsNotEmpty()
     email: string;
     
+    @ApiProperty({example: "your password"})
     @IsString()
     @IsNotEmpty()
     @MinLength(6)
     password: string;
 
 
+    @ApiProperty({example: "John"})
     @IsString()
     @IsNotEmpty()
     firstName: string;
 
+    @ApiProperty({example: "Cena"})
     @IsString()
     @IsNotEmpty()
     lastName: string;   
 
+    @ApiProperty({example: "Client"})
     @IsEnum(UserRole)
     @IsNotEmpty()
     role: UserRole;
 
+    @ApiProperty({example: "98********"})
     @IsString()
     @IsNotEmpty()
     phoneNumber: string;
+
 }
 
