c4959813cb2370208941dfc661d9f1f7378dea2b
Bibek Ale Magar <magarbibek71@gmail.com>
2025-12-22 22:34:12 +0545

    adds case module in backend

---
+++ casbe/src/common/guards/roles.guard.ts
@@ -0,0 +1,20 @@
+import { Injectable, CanActivate, ExecutionContext, Inject } from "@nestjs/common";    
+import { Reflector } from "@nestjs/core";
+import { ROLES_KEY } from "../decorator/roles.decorator";
+import { UserRole } from "../enums/user-role.enum";
+import { Observable } from "rxjs";
+
+
+@Injectable()
+export class RolesGuard implements CanActivate {
+    constructor (private reflector: Reflector){}
+    canActivate(context: ExecutionContext): boolean {
+        const requiredRoles = this.reflector.getAllAndOverride<UserRole[]>(
+            ROLES_KEY,
+            [context.getHandler(), context.getClass()]
+        )
+        if(!requiredRoles) return true;
+        const {user} = context.switchToHttp().getRequest();
+        return requiredRoles.some((role) => user.role === role)
+    }
+}
