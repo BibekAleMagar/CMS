ca6c55f44d175f173726aaee278752043d5f73e7
Bibek Ale Magar <magarbibek71@gmail.com>
2026-01-03 19:39:47 +0545

    upgardes version adn adds image domain

--- casefe/next.config.ts
+++ casefe/next.config.ts
@@ -1,10 +1,16 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  allowedDevOrigins: ["192.168.1.10"],
+  
   images: {
-    domains: ["res.cloudinary.com"]
-  }
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'res.cloudinary.com',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
